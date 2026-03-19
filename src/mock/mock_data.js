c320a07039540319781b1a5da3167a1c6ffb9f71
cccharli459 <cccharli459@gmail.com>
2026-03-19 23:24:31 +0800

    chore: 更新导航数据 - 2026/3/19 23:24:27

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -481,6 +481,6 @@ export const mockData = {
       ]
     }
   ],
-  "title": "猫猫导航🐱",
-  "_fileSha": "b03d0407ec06ac7f254abf6645ac0425a8e74a92"
+  "title": "鹏的导航🐱",
+  "_fileSha": "ea3f00cba99e7d9e1d5cb7b816f6a7836b8c233e"
 }
